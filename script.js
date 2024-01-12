d190d1eb2989b4adc84576115b17fd39ae1b6d7d
Nicholas Tucker <nstucker42@gmail.com>
2024-01-11 18:51:43 -0800

    Implement basic parsing algorithm

--- script.js
+++ script.js
@@ -20,6 +20,11 @@ buttons.forEach((btn) => {
     });
 });
 
+equals.addEventListener('click', () => {
+    displayContent = parse(displayContent);
+    display.textContent = displayContent;
+})
+
 clear.addEventListener('click', () => {
     displayContent = "";
     display.textContent = displayContent;
@@ -33,22 +38,48 @@ function divide(a, b) {
     return a / b;
 }
 
-// Shunting yard algorithm
+// Shunting Yard algorithm
+// INPUT: space-separated infix expression
+// OUTPUT: space-separated postfix/RPN expression
+// **note: as written, doesn't handle parentheses or exponents**
 function parse(str) {
 
     // split expression into tokens (numbers and operators)
-    arr = str.split(" ");
+    tokenArr = str.split(" ");
     let stack = [];
     let queue = [];
 
-    for(let i = 0; i < arr.length; i++){
-        let token = arr[i];
+    for(let i = 0; i < tokenArr.length; i++){
+        let token = tokenArr[i];
+
         // if token is a number
         if(!isNaN(token)){
             queue.push(token);
+        }
         // if token is an operator
-        } else {
-            
+        else {
+            // while the top of the operator stack has higher precedence
+            while(stack.length > 0 && hasPrecedence(stack[stack.length-1], token)){
+                queue.push(stack.pop());
+            } 
+            stack.push(token);  
         }
     }
+    // push remaining operators to the queue
+    for(let j = stack.length-1; j >= 0; j--){
+        queue.push(stack.pop());
+    }
+
+    return queue.join(" ");
+}
+
+function hasPrecedence(a, b){
+    if((a == "+" || a == "-") && (b == "x" || b == "/")){
+        return false;
+    }
+    return true;
+}
+
+function solveRPN(arr){
+
 }
